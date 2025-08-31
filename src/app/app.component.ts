8800e451bd86e02b3fa6bedd0bc9939ce61fbc51
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-08-31 20:36:47 -0300

    Implementação de Data Binding no Angular: Interpolation, Property Binding e Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',                //Nome da tag do componente
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html', //HTML associado
+  styleUrl: './app.component.css'      //CSS associado
 })
 export class AppComponent {
-  title = 'meu-primeiro-projeto-angular';
+  saudacao = 'Olá';
+  separador = ',';
+  espacamento = ' ';
+  title = 'Mundo';            //title é uma variável que está sendo usada no html
+  subtitle = 'Interpolation';
+  userName = 'João'
+
+  isButtonDisabled = false;
+
+  onButtonClick() {
+    alert('Botão clicado')
+    console.log("Teste")
+    this.isButtonDisabled = !this.isButtonDisabled //true
+  }
 }
